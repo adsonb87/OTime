9b9af38cb6253af976b7e786871bed67e79d43ee
adsonb87 <adsonb87@gmail.com>
2022-04-11 21:53:20 -0300

    Projeto Over Time

---
+++ src/main/java/br/com/OTime/OTimeApplication.java
@@ -0,0 +1,13 @@
+package br.com.OTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/OTime/OTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.OTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
